f5301a1ae707125805d69a11b75b4f1c275081f7
metam <metam@DESKTOP-580PBF1>
2025-04-17 17:43:50 +0200

    Ajoutez des fichiers projet.

---
+++ WindowsFormsAppTest/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsAppTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Chaîne de connexion à la base de données
+            string connectionString = "Server=localhost;Database=test;User Id=root;Password=;";
+            // Requête SQL pour récupérer les noms
+            string query = "SELECT nom FROM personne";
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    lstValeurs.Items.Add(reader["nom"].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex.Message);
+            }
+        }
+    }
+}
